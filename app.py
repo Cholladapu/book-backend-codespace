449cbec34fb36f414ba01b77e42b11cf8831c264
Cholladapu <129528731+Cholladapu@users.noreply.github.com>
2024-03-07 17:52:14 +0000

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    return "<h1>Hello world</h1>"
 
 
 if __name__ == "__main__":
