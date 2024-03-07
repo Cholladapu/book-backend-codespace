950d0338bb470744bbe2a4e854e0393a7092ed94
Cholladapu <129528731+Cholladapu@users.noreply.github.com>
2024-03-07 17:53:45 +0000

    add listbook

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+books=[
+    {"id":1,"title":"Book 1","author":"Author 1"},
+    {"id":2,"title":"Book 2","author":"Author 2"},
+    {"id":3,"title":"Book 3","author":"Author 3"}
+]
 @app.route("/")
 def hello_world():
     return "<h1>Hello world</h1>"
